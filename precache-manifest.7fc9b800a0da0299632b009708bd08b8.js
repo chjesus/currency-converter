a41135a4cd199d526595782a491a062a9bf785f9
Jesus Chacon <chjesussao@gmail.com>
2020-08-09 13:37:16 -0400

    Updates

---
+++ precache-manifest.7fc9b800a0da0299632b009708bd08b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c8fc41c40afa537ade4899ceb7781e",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "f7f9f425909e1ed11f53",
+    "url": "/currency-converter/static/css/main.60fdb7c2.chunk.css"
+  },
+  {
+    "revision": "d039c7476a7b11516e8e",
+    "url": "/currency-converter/static/js/2.2ec1f200.chunk.js"
+  },
+  {
+    "revision": "f7f9f425909e1ed11f53",
+    "url": "/currency-converter/static/js/main.7d4251ea.chunk.js"
+  },
+  {
+    "revision": "2dbb75c0d93405364287",
+    "url": "/currency-converter/static/js/runtime-main.4e462dea.js"
+  }
+]);
